8fb3dc8eb17314bf1b67c7e0ea68b3c02da167f9
Leonardo Tolentino <leonardoleoat@yahoo.com.br>
2022-04-16 03:21:42 -0300

    adding files to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import { Button } from './components/Buttton';
+import { InfoItem } from './components/InfoItem';
+import RestartIcon from './svgs/restart.svg';
+import { GridItemType } from './types/GridItemType';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+
+
+  useEffect(() => {
+    resetAndCreateGrid()
+  },[]);
+
+  const resetAndCreateGrid = () => {
+
+  }
+
+
+  return(
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <img src={logoImage} alt='' width="200"  />
+        </C.LogoLink>  
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>   
+
+        <Button label="Reinicar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+
+      </C.Info>
+      <C.GridArea>
+        <C.Grid>
+          <items />
+        </C.Grid>
+      </C.GridArea>
+    </C.Container>
   );
+
 }
 
 export default App;
